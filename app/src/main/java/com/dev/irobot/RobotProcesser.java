2f3e11b6fc278b43be97d6cdaa376d3c2691c9fa
Jacky <JackyAnngo@gmail.com>
2016-04-09 22:40:43 +0800

    添加注册 注销功能

--- app/src/main/java/com/dev/irobot/RobotProcesser.java
+++ app/src/main/java/com/dev/irobot/RobotProcesser.java
@@ -5,20 +5,27 @@ import com.dev.irobot.handler.EventHandler;
 import com.dev.irobot.handler.HookHandler;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 /**
  * hook处理器,
  * 已经在assets/xposed_init中配置
  */
-public class RobotProcesser implements EventHandler, HookHandler {
+public final class RobotProcesser implements EventHandler, HookHandler {
 
+    private static final Map<String, EventHandler> EVENT_HANDLERS = new HashMap<String, EventHandler>();
+    private static final Map<String, HookHandler>  HOOK_HANDLERS  = new HashMap<String, HookHandler>();
     /**
      * 处理AccessibilityService中的AccessibilityEvent事件
      * @param event
      */
     @Override
     public void handleAccessibilityEvent(AccessibilityEvent event) {
-
+        for(EventHandler handler : EVENT_HANDLERS.values()){
+            handler.handleAccessibilityEvent(event);
+        }
     }
 
     /**
@@ -28,6 +35,42 @@ public class RobotProcesser implements EventHandler, HookHandler {
      */
     @Override
     public void handleLoadPackage(LoadPackageParam loadPackageParam) throws Throwable {
+        for(HookHandler handler : HOOK_HANDLERS.values()){
+            handler.handleLoadPackage(loadPackageParam);
+        }
+    }
 
+    /**
+     * 添加EventHandler
+     * @param name
+     * @param eventHandler
+     */
+    public static void addEventHandler(String name, EventHandler eventHandler){
+        EVENT_HANDLERS.put(name, eventHandler);
+    }
+
+    /**
+     * 添加HookHandler
+     * @param name
+     * @param hookHandler
+     */
+    public static void addHookHandler(String name, HookHandler hookHandler){
+        HOOK_HANDLERS.put(name, hookHandler);
+    }
+
+    /**
+     * 删除EventHandler
+     * @param name
+     */
+    public static void removeEventHandler(String name){
+        EVENT_HANDLERS.remove(name);
+    }
+
+    /**
+     * 删除HookHandler
+     * @param name
+     */
+    public static void removeHookHandler(String name){
+        HOOK_HANDLERS.remove(name);
     }
 }
