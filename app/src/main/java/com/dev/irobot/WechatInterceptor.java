0b2daf27664f0fd17496d2fefae3dbfd74bdb27d
Jacky <JackyAnngo@gmail.com>
2016-04-10 22:00:18 +0800

    hook 定位结果

--- app/src/main/java/com/dev/irobot/WechatInterceptor.java
+++ app/src/main/java/com/dev/irobot/WechatInterceptor.java
@@ -1,6 +1,8 @@
 package com.dev.irobot;
 
 import android.location.GpsStatus;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.wifi.WifiManager;
 import android.telephony.TelephonyManager;
@@ -14,7 +16,9 @@ import de.robv.android.xposed.XposedHelpers;
  * @see TelephonyManager#getCellLocation()
  * @see TelephonyManager#getNeighboringCellInfo()
  * @see WifiManager#getScanResults()
- * @see android.location.LocationManager#getGpsStatus(GpsStatus)
+ * @see LocationManager#getGpsStatus(GpsStatus)
+ * @see LocationListener#onLocationChanged(Location)
+ *
  */
 public class WechatInterceptor implements HookMethodInterceptor{
     @Override
@@ -70,6 +74,18 @@ public class WechatInterceptor implements HookMethodInterceptor{
         });
 
         XposedHelpers.findAndHookMethod(LocationManager.class, "getGpsStatus", GpsStatus.class, new MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                super.afterHookedMethod(param);
+            }
+
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                super.beforeHookedMethod(param);
+            }
+        });
+
+        XposedHelpers.findAndHookMethod(LocationListener.class, "onLocationChanged", Location.class, new MethodHook() {
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                 super.afterHookedMethod(param);
