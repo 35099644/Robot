1025d488f02a0b9bdb1a497c3ffa5ce8ca4d657c
Jacky <JackyAnngo@gmail.com>
2016-04-11 18:48:23 +0800

    清理

--- app/src/main/java/com/dev/irobot/handler/HookLoadPackageHandler.java
+++ app/src/main/java/com/dev/irobot/handler/HookLoadPackageHandler.java
@@ -3,18 +3,15 @@ package com.dev.irobot.handler;
 import com.dev.irobot.subscribe.HookMethodHandlerSubscriber;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
-import java.util.Map;
-
 /**
  * Created by Jacky on 2016/4/10.
  */
 public final class HookLoadPackageHandler {
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
         HookMethodHandlerSubscriber subscriber = HookMethodHandlerSubscriber.getInstance();
-        for (Map.Entry<String,HookMethodHandler> entity: subscriber.getHookMethodHandlers().entrySet()) {
-            if(loadPackageParam.packageName.equals(entity.getKey())){
-                entity.getValue().findAndHookMethod();
-            }
+        HookMethodHandler hookMethodHandler = subscriber.getHookMethodHandlers().get(loadPackageParam.packageName);
+        if(hookMethodHandler !=null ){
+            hookMethodHandler.findAndHookMethod();
         }
     }
 }
