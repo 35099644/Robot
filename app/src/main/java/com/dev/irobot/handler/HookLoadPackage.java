cac10c9bac08a7985d35ee3735afb4189da761bc
Jacky <JackyAnngo@gmail.com>
2016-04-13 23:23:46 +0800

    打印log

--- app/src/main/java/com/dev/irobot/handler/HookLoadPackage.java
+++ app/src/main/java/com/dev/irobot/handler/HookLoadPackage.java
@@ -1,6 +1,5 @@
 package com.dev.irobot.handler;
 
-import android.util.Log;
 import com.dev.irobot.subscribe.HookMethodHandlerSubscriber;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
@@ -11,7 +10,6 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
  * 已经在assets/xposed_init中配置
  */
 public final class HookLoadPackage implements IXposedHookLoadPackage {
-    private static final String TAG = HookLoadPackage.class.getSimpleName();
     /**
      * hook并拦截方法, 如果hook了新方法必须重启一次才会生效.
      * @param loadPackageParam
@@ -19,7 +17,6 @@ public final class HookLoadPackage implements IXposedHookLoadPackage {
      */
     @Override
     public void handleLoadPackage(LoadPackageParam loadPackageParam) throws Throwable {
-        Log.v(TAG,"load package:"+loadPackageParam.packageName);
         HookMethodHandlerSubscriber subscriber = HookMethodHandlerSubscriber.getInstance();
         HookMethodHandler hookMethodHandler = subscriber.getHookMethodHandlers().get(loadPackageParam.packageName);
         if(hookMethodHandler !=null ){
