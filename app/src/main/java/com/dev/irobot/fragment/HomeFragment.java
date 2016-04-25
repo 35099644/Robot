0924ca5bcfb0beb30bc1528d0707c522d6472eb2
Jacky <JackyAnngo@gmail.com>
2016-04-25 22:00:38 +0800

    添加poi 解析excel

--- app/src/main/java/com/dev/irobot/fragment/HomeFragment.java
+++ app/src/main/java/com/dev/irobot/fragment/HomeFragment.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import com.dev.irobot.R;
 import com.dev.irobot.support.SupportFragment;
 
 public class HomeFragment extends SupportFragment {
+    private Button importToContacts;
+    private Button importToWeixin;
     @Override
     public void onHiddenChanged(boolean hidden) {
         super.onHiddenChanged(hidden);
@@ -15,9 +18,33 @@ public class HomeFragment extends SupportFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.view_home, container, false);
+        View view = inflater.inflate(R.layout.view_home, container, false);
+
+        init(view);
+        return view;
     }
 
+    private void init(View view) {
+        importToContacts = (Button) view.findViewById(R.id.importToContacts);
+        importToContacts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                importMobileNumberToMobile();
+            }
+        });
+
+        importToWeixin = (Button) view.findViewById(R.id.importToWeixin);
+        importToWeixin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void importMobileNumberToMobile(){
+
+    }
 
 
 }
